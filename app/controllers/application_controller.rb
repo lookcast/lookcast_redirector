a51a2ecfd306507c46b76fa006c7e82a95ec6243
Valentin Litvak <contact@vallitvak.com>
2014-02-11 10:10:37 -0600

    added a redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :redirect_to_lookcast_heroku
+
+  def redirect_to_lookcast_heroku
+    redirect_to "https://signup.lookcast.com/signup?tier=1"
+  end
 end
