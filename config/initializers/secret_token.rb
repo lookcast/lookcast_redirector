e2866e951a36b7f852a60b35c121ae98e09cf212
Valentin Litvak <contact@vallitvak.com>
2014-02-11 10:08:49 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LookcastRedirector::Application.config.secret_token = '612c9887d46859ec5820eabbb5f775a3e1c2bf54aa0e4b8283001f082d70ed6ef25cb8c4425c4b91a1c4b68d282f099451ac14b4b11ac3ddb14a876ee864288c'
